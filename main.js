324e106d2a87c17175ac656b7d7c01c41f539735
ManvitSai1 <92364927+ManvitSai1@users.noreply.github.com>
2024-05-04 17:41:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
-
+scoreLeftWrist=0;
+scoreRightWrist=0;
 function preload()
 {
     song = loadSound("music.mp3");
@@ -15,8 +16,8 @@ function setup()
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
-    poseNet = ml5.poseNet(video, modelLoaded());
-    poseNet.on('pose', gotPoses());
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function modelLoaded()
 {
